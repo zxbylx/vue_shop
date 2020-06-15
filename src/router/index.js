f88f495293ef951efd8c1de00793529413164351
赵新博 <489676437@qq.com>
2020-06-15 21:24:06 +0800

    完成用户列表功能开发

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 
 Vue.use(VueRouter)
 
@@ -13,12 +15,26 @@ const routes = [{
     component: Login
 }, {
     path: '/home',
-    component: Home
+    component: Home,
+    redirect: '/welcome',
+    children: [{
+        path: '/welcome',
+        component: Welcome
+    }, {
+        path: '/users',
+        component: Users
+    }]
 }]
 
 const router = new VueRouter({
     routes
 })
+
+// 解决ElementUI导航栏中的vue-router在3.0版本以上重复点菜单报错问题
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+    return originalPush.call(this, location).catch(err => err)
+}
 
 // 创建路由导航守卫
 router.beforeEach((to, from, next) => {
